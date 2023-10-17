e8db4332a1917747687cbfe0d51c7fe7022474e8
Abhijeet-Bhosale <124586304+Abhijeet-Bhosale@users.noreply.github.com.>
2023-10-17 15:00:12 +0530

    Added two more functionalities

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
-# todo_list.py
+# todo_list_v2.py
+
 tasks = []
 
 def add_task(task):
@@ -15,12 +16,27 @@ def mark_completed(task_index):
     else:
         print("Invalid task index!")
 
+#Added functionalities
+def edit_task(task_index, new_task):
+    if 0 <= task_index < len(tasks):
+        tasks[task_index]["task"] = new_task
+    else:
+        print("Invalid task index!")
+
+def delete_task(task_index):
+    if 0 <= task_index < len(tasks):
+        del tasks[task_index]
+    else:
+        print("Invalid task index!")
+
 if __name__ == "__main__":
     while True:
         print("\nTo-Do List")
         print("1. Add Task")
         print("2. List Tasks")
         print("3. Mark Task as Completed")
+        print("4.Edit Task")
+        print("5.Delete Task")
         print("0. Exit")
 
         choice = input("Enter your choice: ")
@@ -33,7 +49,21 @@ if __name__ == "__main__":
         elif choice == "3":
             task_index = int(input("Enter the task index to mark as completed: ")) - 1
             mark_completed(task_index)
+        # Added functionalities ...
+        elif choice == "4":
+            task_index = int(input("Enter the task index to edit: ")) - 1
+            new_task = input("Enter the new task description: ")
+            edit_task(task_index, new_task)
+        elif choice == "5":
+            task_index = int(input("Enter the task index to delete: ")) - 1
+            delete_task(task_index)
         elif choice == "0":
             break
+        
         else:
             print("Invalid choice. Please try again.")
+
+
+
+
+
